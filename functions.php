55a6cf627fe36dbdc290f9d5f32d81fae79196f1
Brent Stradling <brent.stradling@gmail.com>
2017-02-24 15:33:44 -0600

    added contact form

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@ function reamiddletondds_theme_setup(){
 	register_nav_menu('primary', 'Primary Header Navigation');
 	register_nav_menu('secondary', 'Footer Navigation');
 	add_theme_support('custom-header');
+	add_theme_support('post-thumbnails');
 	add_theme_support('custom-logo', array(
 		'height'      => 100,
 		'width'       => 100,
--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 		if( have_posts() ):
 
 			while( have_posts() ): the_post(); ?>
+				<img src="<?php the_post_thumbnail(); ?>" />
 				<h1><?php the_title(); ?></h1>
 
 			</h4><?php the_content(); ?></h4>
@@ -12,5 +13,42 @@
 
 		endif;
 	?>
+</section>
+<section>
+	<form class="contact-form" action="index.html" method="post">
+		<div class="contact-details">
+			<div class="contact-name">
+				<label>Name</label>
+				<input type="text" name="Name" required>
+			</div>
+			<div class="contact-email">
+				<label>Email</label>
+				<input type="email" name="Email" required>
+			</div>
+			<div class="contact-phone">
+				<label>Phone</label>
+				<input type="tel" name="Phone" required>
+			</div>
+			<div class="contact-date">
+				<label>Requested Date</label>
+				<input type="date" name="Requested Date" value="<?php echo date("Y-m-d");?>" placeholder="requested date">
+			</div>
+		</div>
+		<div class="contact-details">
+			<label>Services Desired</label>
+		<div class="contact-services">
+				<span><input type="checkbox" name="cleaning">Cleaning</span>
+				<span><input type="checkbox" name="cosmetic">Cosmentic</span>
+				<span><input type="checkbox" name="repair">Repair</span>
+			</div>
+			<div class="contact-textbox">
+			<label>Additional Details</label>
+			<textarea name="Additional Info"></textarea>
+		</div>
+	</div>
+	<span>Returning Client? <input type="checkbox" name="returning client">
+	</span>
+	<input type="submit" name="" value="Submit">
+	</form>
 </section>
 <?php get_footer(); ?>
